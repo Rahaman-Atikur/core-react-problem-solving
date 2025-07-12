380af2c4457849586ac8cca2376b9204d8117309
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-07-12 22:48:56 +0100

    Problem 3 is not completed

--- src/Components/ControledInput.jsx
+++ src/Components/ControledInput.jsx
@@ -2,12 +2,12 @@ import { useState } from "react"
 
 export default function ControlledIn() {
     const [text, settext] = useState("");
-    const handleUserInput = (e)=>{
-       settext(e.target.val);
+    const handleUserInput = (e) => {
+        settext(e.target.val);
     }
     return (
         <div>
-            <input  onChange={handleUserInput} placeholder="type" input= "text" />
+            <input value={text} onChange={handleUserInput} placeholder="Please Write something..." type="text" />
             <p>You Typed:{text}</p>
         </div>
     )
