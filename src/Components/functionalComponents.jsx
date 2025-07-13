ec041a86b30be07a1fcbe39096977eb4c36d2daa
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-07-13 09:26:48 +0100

    Medioum is running

--- src/Components/functionalComponents.jsx
+++ src/Components/functionalComponents.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 export default function FunCompo() {
+    const [userWrittenValue, setUseWrittenValue] = useState("");
+    const handleClick = (e) => {
+        let inputValue = e.target.value;
+        setUseWrittenValue(inputValue);
+    }
 
     return (
         <div>
             <input placeholder="Enter the Text..." type="text" /><br />
-            <button>ADD</button>
+            <button onClick={handleClick}>ADD</button>
+            <ul>
+                <li>{userWrittenValue}</li>
+            </ul>
         </div>
     )
 }
