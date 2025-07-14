53ad80ea1ddd8482ed0eb4520cb0ba58306fb7da
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-07-14 19:20:54 +0100

    Problem Solved

--- src/Components/todoList.jsx
+++ src/Components/todoList.jsx
@@ -1,18 +1,17 @@
 import { useState } from "react"
-
 export default function TodoList() {
     const [currentList, setCurrentList] = useState([]);
     const [inputText, setInputText] = useState("");
     const handleInputChange = (e) => {
         let userTypedWriting = e.target.value;
-
+        setInputText(userTypedWriting);
     }
     const handleAdd = () => {
         setCurrentList(...currentList, inputText);
         setInputText("");
     }
     const handleRemove = () => {
-
+        currentList("");
     }
     return (
         <div>
